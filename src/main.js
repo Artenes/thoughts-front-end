8e1ced9e944eb9caa549d75714288ffa96c90ce4
Artenes Nogueira <artenesama@gmail.com>
2017-10-06 21:30:09 -0300

    Add route protection for authenticated users

--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import auth from '@/utils/auth';
 
 Vue.config.productionTip = false
 
+const protectedRoutes = ['feed', 'me', 'pseudonym'];
+
+router.beforeEach((to, from, next) => {
+
+    if (!auth.check() && protectedRoutes.includes(to.name))
+        return next('/');
+
+    return next();
+
+});
+
+auth.onLogout(function () {
+
+    router.push('/');
+
+});
+
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: {App}
 })
